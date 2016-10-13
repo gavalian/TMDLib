2fdc0fee87bd2d4c92b15b569179f7feae3b8fbf
gavalian <gavalian@meson.jlab.org>
2016-10-13 13:15:38 -0400

    added a welcome class

---
+++ grid/src/main/java/org/jlab/grid/utils/Welcome.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jlab.grid.utils;
+
+/**
+ *
+ * @author gavalian
+ */
+public class Welcome {
+    public Welcome(){
+        System.out.println("Welcome to grid");
+    }
+}
